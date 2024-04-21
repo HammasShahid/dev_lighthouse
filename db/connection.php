398464ab202a620b6f2d667571650633e8f42144
Hammas <hammasshahid11@gmail.com>
2024-04-22 02:28:20 +0500

    extract variables from connection string

--- db/connection.php
+++ db/connection.php
@@ -1,6 +1,14 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=lighthouse', 'root', '');
-// $pdo = new PDO('mysql:host=localhost;port=3306;dbname=makixdjo_lighthouse', 'makixdjo_lighthouse', 'makixdjo_lighthouse');
+
+$host = "localhost";
+$port = "3306";
+$dbname = "makixdjo_lighthouse";
+$username = "makixdjo_lighthouse";
+$password = "makixdjo_lighthouse";
+
+$pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password);
+// $pdo = new PDO('mysql:host=localhost;port=3306;dbname=lighthouse', 'root', '');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 
 return $pdo;
